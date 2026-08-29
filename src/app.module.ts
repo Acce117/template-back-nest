9d8d7b003f79129881c2771560cac44afab2d9f6
Acce117 <e.carralero117@gmail.com>
2026-08-29 15:27:17 -0400

    refactor: wire ALS middleware, AuthModule and /api prefix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
-import { Module } from "@nestjs/common";
-import { SiteModule } from "./site/site.module.js";
+import { MiddlewareConsumer, Module } from "@nestjs/common";
+import { AuthModule } from "./modules/auth/auth.module.js";
 import { CommonModule } from "./common/common.module.js";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import databaseConfig from "./config/database.config.js";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { UsersModule } from "./users/users.module.js";
-import { RouterModule } from "@nestjs/core";
-import { routes } from "./routes/routes.js";
+import { UsersModule } from "./modules/users/users.module.js";
 import { ThrottlerModule } from "@nestjs/throttler";
 import { BullModule } from "@nestjs/bullmq";
 import { SendMailModule } from "./mailer/sendMail.module.js";
@@ -15,6 +13,13 @@ import { JwtModule } from "@nestjs/jwt";
 import { CacheModule } from "@nestjs/cache-manager";
 import cacheConfig from "./config/cache.config.js";
 import bullmqConfig from "./config/bullMQ.config.js";
+import { ScheduleModule } from "@nestjs/schedule";
+import { AsyncLocalStorage } from "async_hooks";
+import { Request } from "express";
+import { AUTH_COOKIE_NAME } from "./common/cookies/cookie.helper.js";
+import { decode } from "jsonwebtoken";
+import { CaslModule } from "./integrations/casl/casl.module.js";
+import { ALSModule } from "./integrations/als/als.module.js";
 
 @Module({
     imports: [
@@ -35,13 +40,35 @@ import bullmqConfig from "./config/bullMQ.config.js";
             inject: [ConfigService],
         }),
         JwtModule,
+        ALSModule,
+        ScheduleModule.forRoot(),
 
         //project modules
-        SiteModule,
+        AuthModule,
         UsersModule,
+        CaslModule,
         CommonModule,
-        RouterModule.register(routes),
         SendMailModule,
     ],
 })
-export class AppModule {}
+export class AppModule {
+    constructor(private readonly als: AsyncLocalStorage<any>) {}
+
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply((req: Request, res, next) => {
+                const jwt = req.cookies?.[AUTH_COOKIE_NAME];
+
+                const store = new Map<string, any>();
+
+                if (jwt) {
+                    const payload = decode(jwt) as { id?: string } | null;
+
+                    if (payload?.id) store.set("userId", payload.id);
+                }
+
+                this.als.run(store, () => next());
+            })
+            .forRoutes("*path");
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,39 @@
 import { ValidationPipe } from "@nestjs/common";
 import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import cookieParser from "cookie-parser";
 import { AppModule } from "./app.module.js";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { ErrorFilter } from "./common/filters/exception.filter.js";
+import helmet from "helmet";
 
 async function bootstrap() {
     const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    app.setGlobalPrefix("/api");
+
     app.set("query parser", "extended");
 
-    app.enableCors(/** configure as you require */);
+    app.use(cookieParser());
+    app.use(helmet());
+
+    app.enableCors({
+        origin: process.env.FRONT_BASE_URL,
+        credentials: true,
+        methods: ["GET", "POST", "PATCH", "DELETE"],
+        maxAge: 3600,
+    });
 
     app.useGlobalPipes(
         new ValidationPipe({
             whitelist: true,
             forbidNonWhitelisted: true,
+            transform: true,
         }),
     );
 
     const httpAdapter = app.get(HttpAdapterHost);
     app.useGlobalFilters(new ErrorFilter(httpAdapter));
+
+    app.enableShutdownHooks();
 
     await app.listen(parseInt(process.env.DOMAIN));
 }
